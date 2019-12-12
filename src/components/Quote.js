a8e210fcd68f4c96ec57c2fd17bd34dab29460fb
msearles25 <skaman4459@gmail.com>
2019-12-12 18:05:22 -0500

    mvp

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,18 +3,21 @@ import React from 'react';
 // redux
 import { connect } from 'react-redux';
 
-import  { fetchQuote } from '../actions/quoteActions';
+import { fetchQuote } from '../actions/quoteActions';
 
 import Loader from 'react-loader-spinner'
 
 const Quote = props => {
     return (
-        <div>
+        <div className='quoteBox'>
             {console.log(props)}
             <h1>A Penny for your Quotes!</h1>
-            {props.loading &&  <Loader type="Puff" color="#00BFFF" height={100} width={100} />}
-            {props.quote && !props.loading && <><p>{props.quote}</p><p>- {props.author}</p></>}
-            <button onClick={props.fetchQuote}>Click for a Quote</button>
+            <div className='quote'>
+                {!props.loading && !props.quote && <p>Get a quote!</p>}
+                {props.loading && <Loader type="Puff" color="#b95f2a" height={50} width={50} />}
+                {props.quote && !props.loading && <><p>{props.quote}</p><p>- {props.author}</p></>}
+            </div>
+            <button className='btn' onClick={props.fetchQuote}>Click for a Quote</button>
         </div>
     )
 }
@@ -29,6 +32,6 @@ const mapStateToProps = state => {
 }
 
 export default connect(
-    mapStateToProps, 
+    mapStateToProps,
     { fetchQuote })
     (Quote);
